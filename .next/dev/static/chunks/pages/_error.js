b5a309b5d0bf00622c0b18046e92284d404580e9
hi <ahirsarkar2022@gmail.com>
2026-06-22 01:20:48 +0530

    Refactor code structure for improved readability and maintainability

---
+++ .next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/0fml_next_dist_compiled_1aaluh5._.js",
+  "static/chunks/0fml_next_dist_shared_lib_0jp-1we._.js",
+  "static/chunks/0fml_next_dist_client_01esnuv._.js",
+  "static/chunks/0fml_next_dist_008ejnr._.js",
+  "static/chunks/1yxp_next_dist_pages__error_1qor1ky.js",
+  "static/chunks/[next]_entry_page-loader_ts_147iqq_._.js",
+  "static/chunks/0fml_react-dom_028dao8._.js",
+  "static/chunks/0fml_1sbl3a4._.js",
+  "static/chunks/[root-of-the-server]__0ad1mee._.js",
+  "static/chunks/[project]_Projects_x7710_app_pages__error_0du2_q-._.js",
+  "static/chunks/turbopack-[project]_Projects_x7710_app_pages__error_1uzp_5r._.js"
+])
