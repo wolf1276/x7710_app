4c657f3c396e5d8f7b09b3da3dfbaa9d5bff7c52
wolf1276 <ahirsarkar2022@gmail.com>
2026-06-23 13:12:19 +0530

    Refactor code structure for improved readability and maintainability

---
+++ .next/dev/static/chunks/[project]_deployments_x7710-app_pages__error_0du2_q-._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/0eo2_next_dist_compiled_08r65c6._.js","static/chunks/0eo2_next_dist_shared_lib_11n4b2m._.js","static/chunks/0eo2_next_dist_client_1dn0e3b._.js","static/chunks/0eo2_next_dist_1losajx._.js","static/chunks/1q_m_next_dist_pages__error_0ejno1n.js","static/chunks/[next]_entry_page-loader_ts_1tccon4._.js","static/chunks/0eo2_react-dom_038b3tx._.js","static/chunks/0eo2_1ra2cnd._.js","static/chunks/[root-of-the-server]__1ow9wo5._.js"],
+    source: "entry"
+});
--- .next/dev/static/chunks/pages/_error.js
+++ .next/dev/static/chunks/pages/_error.js
@@ -1,13 +1,13 @@
 __turbopack_load_page_chunks__("/_error", [
-  "static/chunks/0fml_next_dist_compiled_1aaluh5._.js",
-  "static/chunks/0fml_next_dist_shared_lib_0jp-1we._.js",
-  "static/chunks/0fml_next_dist_client_01esnuv._.js",
-  "static/chunks/0fml_next_dist_008ejnr._.js",
-  "static/chunks/1yxp_next_dist_pages__error_1qor1ky.js",
-  "static/chunks/[next]_entry_page-loader_ts_147iqq_._.js",
-  "static/chunks/0fml_react-dom_028dao8._.js",
-  "static/chunks/0fml_1sbl3a4._.js",
-  "static/chunks/[root-of-the-server]__0ad1mee._.js",
-  "static/chunks/[project]_Projects_x7710_app_pages__error_0du2_q-._.js",
-  "static/chunks/turbopack-[project]_Projects_x7710_app_pages__error_1uzp_5r._.js"
+  "static/chunks/0eo2_next_dist_compiled_08r65c6._.js",
+  "static/chunks/0eo2_next_dist_shared_lib_11n4b2m._.js",
+  "static/chunks/0eo2_next_dist_client_1dn0e3b._.js",
+  "static/chunks/0eo2_next_dist_1losajx._.js",
+  "static/chunks/1q_m_next_dist_pages__error_0ejno1n.js",
+  "static/chunks/[next]_entry_page-loader_ts_1tccon4._.js",
+  "static/chunks/0eo2_react-dom_038b3tx._.js",
+  "static/chunks/0eo2_1ra2cnd._.js",
+  "static/chunks/[root-of-the-server]__1ow9wo5._.js",
+  "static/chunks/[project]_deployments_x7710-app_pages__error_0du2_q-._.js",
+  "static/chunks/turbopack-[project]_deployments_x7710-app_pages__error_0rilt8k._.js"
 ])
